a9950c2cd6d0976ce815b60e222af01f4e851dc4
Greg <greggordon98@gmail.com>
2018-11-15 13:33:13 -0800

    first comit
    first comit

--- js/Game.js
+++ js/Game.js
@@ -4,14 +4,29 @@
 // and removing a life counter.
 
 class Game {
-
+  var phrases = [];
   function Misphrase(missed, phrases) {
     constructor() {
       this.missed = missed;
       this.phrases = phrases;
     }
-
   }
 
+  //this method randomly retrieves one of the phrases stored in the phrases array.
+  getRandomPhrase() {
+    let rando = phrases[Math.floor(Math.random() * phrases.length)];
+  }
+
+
+  //this method checks to see if the button clicked by the player
+  //matches a letter in the phrase.
+  handleInteraction() {
+    if(button.click() === phrases.forEach()){
+      showMatchedLetter(phrases);
+      checkForWin();
+    } else {
+      removeLife();
+    }
+  }
 
 }
