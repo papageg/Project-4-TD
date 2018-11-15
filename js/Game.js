7e5d31e71b37f415c0e8cd31fc2a22d1d0e084cd
Greg <greggordon98@gmail.com>
2018-11-15 13:10:44 -0800

    Initial commit

---
+++ js/Game.js
@@ -0,0 +1,17 @@
+//create a Game class with methods for starting
+//and ending the game, handling interactions,
+//getting random phrases, checking for a win,
+// and removing a life counter.
+
+class Game {
+
+  function Misphrase(missed, phrases) {
+    constructor() {
+      this.missed = missed;
+      this.phrases = phrases;
+    }
+
+  }
+
+
+}
---
+++ js/app.js
@@ -0,0 +1,2 @@
+// Perform basic DOM selection, add event handlers,
+//and to reset the game when it ends
