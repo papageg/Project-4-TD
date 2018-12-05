b2fdf30f8b0d01afb8f0bbd9fa230b08d4299e67
Greg <greggordon98@gmail.com>
2018-12-05 10:40:52 -0800

    error
    error

--- js/Game.js
+++ js/Game.js
@@ -3,15 +3,12 @@
 //getting random phrases, checking for a win,
 // and removing a life counter.
 //create game class
-
 class Game {
-
 	constructor(missed, phrases) {
 		this.missed = missed;
 		this.phrases = phrases;
 		this.currentPhrase = null;
 	}
-
 	//this method randomly retrieves one of the phrases stored in the phrases array.
 	getRandomPhrase() {
 		const randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
@@ -20,37 +17,33 @@ class Game {
 	//this method checks to see if the button clicked by the player
 	//matches a letter in the phrase.
 	//Try a switch statment??
-	 handleInteraction(button) {
-	  if (this.currentPhrase.textContent === button.textContent) {
-	      this.currentPhrase.showMatchedLetter();
-	      this.checkForWin();
-	    } else {
-	      this.removeLife();
-	    }
+	handleInteraction(button) {
+		if (phrase.textContent === button.textContent) {
+			this.currentPhrase.showMatchedLetter();
+			this.checkForWin();
+		} else {
+			this.removeLife();
+		}
 	}
 	//this method removes a life, removes a heart from the board, and,
 	//if the player is out of lives, ends the game.
 	removeLife() {
-	// 	if (scoreboard <= 1) {
-  //     scoreboard.remove(scoreboard[0]);
-	// }
-	// else {
-	// 	alert('Game Over!');
-	// }
-}
-
-	//this method checks to see if the player has selected all of the letters.
-	checkForWin() {
-
+		// 	if (scoreboard <= 1) {
+		//     scoreboard.remove(scoreboard[0]);
+		// }
+		// else {
+		// 	alert('Game Over!');
+		// }
 	}
+	//this method checks to see if the player has selected all of the letters.
+	checkForWin() {}
 	//this method displays a message if the player
 	// wins or a different message if they lose.
 	gameOver() {}
 	//calls the getRandomPhrase() method, and adds that phrase to the board
 	// by calling the Phrase class' addPhraseToDisplay() method.
-
 	startGame() {
-  	this.currentPhrase = new Phrase(this.getRandomPhrase());
+		this.currentPhrase = new Phrase(this.getRandomPhrase());
 		this.currentPhrase.addPhraseToDisplay();
 	}
 }
--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
-
 let game = new Game;
- const phrases = ['this is first', 'this is second', 'this is third'];
-
-
+const phrases = ['this is first', 'this is second', 'this is third'];
 //this function hides the start screen overlay.
 function resetDisplay() {
-  document.getElementById("overlay").style.display = "none";
+	document.getElementById("overlay").style.display = "none";
 }
 //this function is called when a player selects a letter.
 //It disables the button on the onscreen keyboard and calls
@@ -16,49 +13,38 @@ function resetDisplay() {
 //     game.handleInteraction()
 //     });
 //   }
-
-
 /***************************************************************/
 /////////////////////////////////////////////////////////////////
 // Get all the buttons
 const buttons = document.querySelectorAll('.key');
-
 //loop through buttons to add event listener to each one
 for (let button of buttons) {
-	button.addEventListener('click', function(){
+	button.addEventListener('click', function() {
 		//call markButton method and pass in the button
-    markButton(button);
+		markButton(button);
 	})
 }
-
 markButton(button) {
 	// disable the button
 	// pass the button to the game.handleInteraction() method
-  button.disabled = true;
-  game.handleInteraction(button)
+	button.disabled = true;
+	game.handleInteraction(button)
 }
-
-
 // inside the game.handleInteraction() method, use .textContent to get the value of the letter inside the button that the user intended to click.
 // send this string value to the phrase.checkLetter() method
 /////////////////////////////////////////////////////////////////
 /***************************************************************/
-
-
-
-
 //Add an event listener to the "Start Game" button which calls
 // the resetDisplay() function, creates a new Game object,
 // and starts the game.
 //Add event listeners to each of the keyboard buttons, so that clicking
 // a button calls the markButton() function.
-
 //Add an event listener to the "Start Game" button which calls
 // the resetDisplay() function, creates a new Game object,
 // and starts the game.
 document.getElementById("btn__reset").addEventListener("click", (e) => {
-  markButton();
-   resetDisplay();
-   game = new Game(0, phrases);
-   game.startGame();
+	markButton();
+	resetDisplay();
+	game = new Game(0, phrases);
+	game.startGame();
 });
